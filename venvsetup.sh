fcf7d6064c4089b63031bb9a19fe529109aa0c14
Kenny Bui <63689042+kennybui-data-ai@users.noreply.github.com>
2021-01-24 13:55:09 -0500

    Add files via upload

---
+++ venvsetup.sh
@@ -0,0 +1,4 @@
+export WORKON_HOME=~/Envs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+source /usr/local/bin/virtualenvwrapper.sh
